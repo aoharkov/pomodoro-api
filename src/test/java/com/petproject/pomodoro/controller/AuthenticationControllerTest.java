6e57eaa17b9e93b2bf9bfaf98e17931e9b0fd607
Artem Oharkov <aoharkov@kse.org.ua>
2020-06-18 12:09:06 +0300

    fix AuthenticationControllerTest

--- src/test/java/com/petproject/pomodoro/controller/AuthenticationControllerTest.java
+++ src/test/java/com/petproject/pomodoro/controller/AuthenticationControllerTest.java
@@ -1,43 +1,38 @@
 package com.petproject.pomodoro.controller;
 
 import com.petproject.pomodoro.config.jwt.JwtProcessor;
-import com.petproject.pomodoro.entity.Role;
 import com.petproject.pomodoro.web.dto.JwtRequestDto;
-import com.petproject.pomodoro.web.dto.JwtResponseDto;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
-import java.util.ArrayList;
+import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class AuthenticationControllerTest {
-
-    private String TOKEN_STUB = "token-stub";
-    private String USERNAME = "user";
+    private static final String TOKEN_STUB = "token-stub";
+    private static final String USERNAME = "user";
+    private static final String PASSWORD = "password";
 
     @InjectMocks
     private AuthenticationController instance;
 
     @Mock
-    private JwtProcessor jwtProcessor;
+    private AuthenticationManager authenticationManager;
 
     @Mock
-    private AuthenticationManager authenticationManager;
+    private JwtProcessor jwtProcessor;
 
     @Mock
     private UserDetailsService userDetailsService;
@@ -48,24 +43,18 @@ class AuthenticationControllerTest {
     @Mock
     private UserDetails userDetails;
 
-    private JwtRequestDto jwtRequestDto = new JwtRequestDto(USERNAME, USERNAME);
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.initMocks(this);
-    }
+    @Mock
+    private JwtRequestDto jwtRequestDto;
 
     @Test
     void authenticate() {
-        when(authenticationManager.authenticate(authenticationToken)).thenReturn(authenticationToken);
-        ArrayList authorities = new ArrayList<Role>();
-        authorities.add(Role.USER);
-
-        when(userDetails.getAuthorities()).thenReturn(authorities);
+        when(jwtRequestDto.getUsername()).thenReturn(USERNAME);
+        when(jwtRequestDto.getPassword()).thenReturn(PASSWORD);
+        when(authenticationManager.authenticate(any())).thenReturn(authenticationToken);
         when(userDetailsService.loadUserByUsername(anyString())).thenReturn(userDetails);
+        when(userDetails.getAuthorities()).thenReturn(Collections.emptyList());
+        when(jwtProcessor.createJwt(USERNAME, Collections.emptyList())).thenReturn(TOKEN_STUB);
 
-        when(jwtProcessor.createJwt(USERNAME, authorities)).thenReturn(TOKEN_STUB);
-
-        assertEquals(instance.authenticate(jwtRequestDto), ResponseEntity.ok(new JwtResponseDto(TOKEN_STUB)));
+        assertEquals(TOKEN_STUB, instance.authenticate(jwtRequestDto).getBody().getToken());
     }
 }
