e16a0dce63a85bd8eea5617067c5db12787a4fe7
Maksym Ivanchenko <maksym_ivanchenko@epam.com>
2020-06-22 14:57:54 +0300

    Change response status "FOUND" to "OK" in UserController

--- src/main/java/com/petproject/pomodoro/controller/UserController.java
+++ src/main/java/com/petproject/pomodoro/controller/UserController.java
@@ -9,7 +9,14 @@ import io.swagger.annotations.ApiImplicitParams;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -66,7 +73,7 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<User>> readAllUsers() {
         return ResponseEntity
-                .status(HttpStatus.FOUND)
+                .status(HttpStatus.OK)
                 .body(userService.findAll());
     }
 
@@ -84,7 +91,7 @@ public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<User> readUserById(@PathVariable Long id) {
         return ResponseEntity
-                .status(HttpStatus.FOUND)
+                .status(HttpStatus.OK)
                 .body(userService.findById(id));
     }
 
@@ -102,7 +109,7 @@ public class UserController {
     @GetMapping("/{id}/pomodoros")
     public ResponseEntity<List<Pomodoro>> readAllPomodoros(@PathVariable Long id) {
         return ResponseEntity
-                .status(HttpStatus.FOUND)
+                .status(HttpStatus.OK)
                 .body(pomodoroService.findByUserId(id));
     }
 
